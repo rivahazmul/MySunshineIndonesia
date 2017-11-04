eb317ed1693b8d01b835e33983b3688e05e0e0fe
rivahazmul <riva.hazmul@gmail.com>
2017-11-04 13:34:35 +0700

    Binding view layout to java

--- app/src/main/java/com/riva/example/mysunshineindonesia/MainActivity.java
+++ app/src/main/java/com/riva/example/mysunshineindonesia/MainActivity.java
@@ -2,12 +2,28 @@ package com.riva.example.mysunshineindonesia;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView weatherDayTitle;
+    ImageView ImgWeather;
+    TextView weatherDesc;
+    TextView weatherTemperature;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        weatherDayTitle= (TextView) findViewById(R.id.weather_day_title);
+        ImgWeather= (ImageView) findViewById(R.id.img_weather);
+        weatherDesc= (TextView) findViewById(R.id.weather_desc);
+        //weatherTemperature= (TextView) findViewById(R.id.);
+
+        weatherDayTitle.setText("Hari Minggu");
+        weatherDesc.setText("cerah sekaleee");
+
+        ImgWeather.setImageResource(R.drawable.ic_launcher_foreground);
+
     }
 }
